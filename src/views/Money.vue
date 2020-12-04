89b08763edd6f82d7e8af1bcf6069c8ebfd64d83
xiaoshu <1324214367@qq.com>
2020-12-04 16:06:14 +0800

    封装 record 的操作

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -20,23 +20,15 @@
     import Types from '@/components/Money/Types.vue';
     import FormItem from '@/components/Money/FormItem.vue';
     import Tags from '@/components/Money/Tags.vue';
-    import {Component,Watch} from 'vue-property-decorator';
-    import recordListModel from '@/models/recordListModel';
+    import {Component} from 'vue-property-decorator';
     import tagListModel from '@/models/tagListModel';
-
-    // import model from '@/model.js';
-    // const model =require ('@/model.js').default
-    // console.log(model)
-
-    const recordList: RecordItem[] = recordListModel.fetch()
-
 
     @Component({
         components: {Tags, FormItem, Types, NumberPad},
     })
     export default class Money extends Vue{
         tags = window.tagList;
-        recordList: RecordItem[]= recordList;
+        recordList = window.recordList;
         record: RecordItem = {
             tags: [], notes: '', type: '-', amount: 0
         }
@@ -55,11 +47,7 @@
             this.record.amount = parseFloat(value)
         }
         saveRecord(){
-            recordListModel.create(this.record)
-        }
-        @Watch('recordList')
-        onChildChanged() {
-           recordListModel.save()
+            window.createRecord(this.record)
         }
     }
 
