ac45ae4de7b9fa98b3a9d03662064cbf64149bbe
xiaoshu <1324214367@qq.com>
2020-12-04 09:10:38 +0800

    重新封装 recordListModel

--- src/components/Money/FormItem.vue
+++ src/components/Money/FormItem.vue
@@ -5,7 +5,7 @@
             <input type="text"
                     :value="value"
                    @input="onChildChanged($event.target.value)"
-                   :placeholder="this.placeholder">
+                   :placeholder="placeholder">
         </label>
     </div>
 </template>
@@ -13,7 +13,7 @@
 
 <script lang="ts">
   import Vue from 'vue';
-  import {Component, Prop, Watch} from 'vue-property-decorator';
+  import {Component, Prop} from 'vue-property-decorator';
 
   @Component
   export default class FormItem extends Vue {
@@ -22,7 +22,6 @@
       @Prop({required:true}) fieldName!: string
       @Prop() placeholder?: string
 
-      @Watch('value')
       onChildChanged(value: string) {
           this.$emit('update:value',value)
       }
