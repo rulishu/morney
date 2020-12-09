3e04e2bca23b4e4c4542500af245908e5724e7c7
xiaoshu <1324214367@qq.com>
2020-12-09 10:36:51 +0800

    完成统计页面

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -4,7 +4,7 @@
         :value.sync="type"/>
         <ol>
             <li v-for="(group,index) in groupedList" :key='index'>
-                <h3 class="title">{{beautify(group.title)}}</h3>
+                <h3 class="title">{{beautify(group.title)}}<span>￥{{group.total}}</span></h3>
                 <ol>
                     <li v-for="item in group.items"  :key='item.id' class="record">
                         <span>{{tagString(item.tags)}}</span>
@@ -55,10 +55,13 @@
         get groupedList(){
             const {recordList}=this
             if(recordList.length===0){return [];}
-            type HashTableValue = {title: string;items: RecordItem[]}
 
-            const newList = clone(recordList).sort((a,b) => dayjs(b.createdAt).valueOf()-dayjs(a.createdAt).valueOf())
-            const result = [{title: dayjs(newList[0].createdAt).format('YYYY-M-D'),items: [newList[0]]}]
+            const newList = clone(recordList)
+                            .filter(r => r.type===this.type)
+                            .sort((a,b) => dayjs(b.createdAt).valueOf()-dayjs(a.createdAt).valueOf())
+
+            type Result = {title: string;items: RecordItem[];total?: number}[]
+            const result: Result = [{title: dayjs(newList[0].createdAt).format('YYYY-M-D'),items: [newList[0]]}]
             for(let i =1;i<newList.length;i++){
                 const current = newList[i];
                 const last = result[result.length-1];
@@ -68,6 +71,7 @@
                     result.push({title: dayjs(current.createdAt).format('YYYY-M-D'),items: [current]})
                 }
             }
+            result.map(group =>{group.total = group.items.reduce((sum,item) => sum+item.amount,0)})
             return result;
         }
         beforeCreate(){
